d10990e729e3bbd24f5901101e3e05a4c7d74644
vincenzocavallaro <vincenzo.cavallaro98@gmail.com>
2026-01-11 13:18:36 +0100

    Fase 1-"Intro"

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("The secret code is prepared: ****.");
+        System.out.println();
+        System.out.println("Turn 1. Answer:");
+        System.out.println("1234");
+        System.out.println("Grade: None.");
+        System.out.println();
+        System.out.println("Turn 2. Answer:");
+        System.out.println("9876");
+        System.out.println("Grade: 4 bulls.");
+        System.out.println("Congrats! The secret code is 9876.");
+
+    }
+}
